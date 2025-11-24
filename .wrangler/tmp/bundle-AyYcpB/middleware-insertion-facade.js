a2d803a6bb6000d0adc4700b5d92ef8ccb7a110f
Tuanhung01z <phamhungpv1104@gmail.com>
2025-11-24 17:22:20 +0700

    Update attributes logic + fix API

--- .wrangler/tmp/bundle-AyYcpB/middleware-insertion-facade.js
+++ .wrangler/tmp/bundle-AyYcpB/middleware-insertion-facade.js
@@ -1,8 +1,8 @@
-				import worker, * as OTHER_EXPORTS from "/Users/tuanhung/Desktop/project/src/worker.js";
+				import worker, * as OTHER_EXPORTS from "/Users/tuanhung/Desktop/project/node_modules/wrangler/templates/pages-shim.ts";
 				import * as __MIDDLEWARE_0__ from "/Users/tuanhung/Desktop/project/node_modules/wrangler/templates/middleware/middleware-ensure-req-body-drained.ts";
 import * as __MIDDLEWARE_1__ from "/Users/tuanhung/Desktop/project/node_modules/wrangler/templates/middleware/middleware-miniflare3-json-error.ts";
 
-				export * from "/Users/tuanhung/Desktop/project/src/worker.js";
+				export * from "/Users/tuanhung/Desktop/project/node_modules/wrangler/templates/pages-shim.ts";
 
 				export const __INTERNAL_WRANGLER_MIDDLEWARE__ = [
 					
--- .wrangler/tmp/bundle-AyYcpB/middleware-loader.entry.ts
+++ .wrangler/tmp/bundle-AyYcpB/middleware-loader.entry.ts
@@ -3,12 +3,12 @@
 // export dynamically through wrangler, or we can potentially let users directly
 // add them as a sort of "plugin" system.
 
-import ENTRY, { __INTERNAL_WRANGLER_MIDDLEWARE__ } from "/Users/tuanhung/Desktop/project/.wrangler/tmp/bundle-SWldIL/middleware-insertion-facade.js";
+import ENTRY, { __INTERNAL_WRANGLER_MIDDLEWARE__ } from "/Users/tuanhung/Desktop/project/.wrangler/tmp/bundle-AyYcpB/middleware-insertion-facade.js";
 import { __facade_invoke__, __facade_register__, Dispatcher } from "/Users/tuanhung/Desktop/project/node_modules/wrangler/templates/middleware/common.ts";
-import type { WorkerEntrypointConstructor } from "/Users/tuanhung/Desktop/project/.wrangler/tmp/bundle-SWldIL/middleware-insertion-facade.js";
+import type { WorkerEntrypointConstructor } from "/Users/tuanhung/Desktop/project/.wrangler/tmp/bundle-AyYcpB/middleware-insertion-facade.js";
 
 // Preserve all the exports from the worker
-export * from "/Users/tuanhung/Desktop/project/.wrangler/tmp/bundle-SWldIL/middleware-insertion-facade.js";
+export * from "/Users/tuanhung/Desktop/project/.wrangler/tmp/bundle-AyYcpB/middleware-insertion-facade.js";
 
 class __Facade_ScheduledController__ implements ScheduledController {
 	readonly #noRetry: ScheduledController["noRetry"];
